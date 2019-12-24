9d08d9074b3142988a43e6ab11d69cee083dcd83
RedSky415 <rokismylove@gmail.com>
2019-12-24 23:14:44 +0200

    fixed

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -2,25 +2,22 @@
 let a = +prompt('Enter a:');
 let b = +prompt('Enter b:');
 let c = +prompt('Enter c:');
-
-
-
-if (isNaN(a) || isNaN(b) || isNaN(c)) 
-    console.log('Invalid input data');
- else {
-   let inD = b*b - 4*a*c;
-    
-    if (inD < 0) {
-        console.log("no solution");
-    } else if (inD === 0) {
-       let  x = - b / (2 * a);
-        console.log("x ="+ x);
-    } else if (inD > 0) {
-    	let D = Math.sqrt(inD);
-        let x1 = (- b + D) / (2 * a);
-        let x2 = (- b - D) / (2 * a);
-        console.log("x1 = " + x1);
-        console.log("x2 = " + x2);
-
-    }
+let koef = 4;
+let koef2 = 2;
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+console.log('Invalid input data'); 
+} else {
+let inD = b*b - koef*a*c;
+if (inD < 0) {
+console.log('no solution');
+} else if (inD === 0) {
+let x = - b / (koef2 * a);
+console.log('x ='+ x);
+} else if (inD > 0) {
+let D = Math.sqrt(inD);
+let x1 = (- b + D) / (koef2 * a);
+let x2 = (- b - D) / (koef2* a);
+console.log('x1 = ' + x1);
+console.log('x2 = ' + x2);
+}
 }
