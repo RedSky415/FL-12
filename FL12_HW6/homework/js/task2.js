ab7816c32081ef96e29677872d5bf2ccd190bb66
RedSky415 <rokismylove@gmail.com>
2019-12-24 23:01:31 +0200

    HW6

---
+++ FL12_HW6/homework/js/task2.js
@@ -0,0 +1,16 @@
+let a = +prompt("Enter a:");
+let b = +prompt("Enter b:");
+let c = +prompt("Enter c:");
+if (isNaN(a) || isNaN(b) || isNaN(c)) 
+    alert("input values should be ONLY numbers");
+ else if (a <= 0 || b <= 0 || c <= 0) 
+    alert("A triangle must have 3 sides with a positive definite length");
+ else if(a + b > c && c + b > a && a + c > b) {
+    if (a === b && a === c && b === c)
+    	console.log("equilateral triangle");
+    else if (a != b && a != c && b != c)
+    	 console.log("scalene triangle");
+    else console.log("isosceles triangle");
+  }
+  else if (a + b < c || c + b < a || a + c < b)
+  	console.log("Triangle doesn’t exist");
